54745e4680dadb4656ce57e9281376eba188442b
Sandor Kepiro <sandor.kepiro@guthand.com>
2026-01-05 17:42:52 +0100

    init: init project with DX tools

--- webapp/controller/SupplierList.controller.ts
+++ webapp/controller/SupplierList.controller.ts
@@ -4,9 +4,6 @@ import Controller from "sap/ui/core/mvc/Controller";
  * @namespace courseproject.controller
  */
 export default class SupplierList extends Controller {
-
-    /*eslint-disable @typescript-eslint/no-empty-function*/
-    public onInit(): void {
-
-    }
+  /*eslint-disable @typescript-eslint/no-empty-function*/
+  public onInit(): void {}
 }
